ea08b02a0b1645dc5148f5514a96597eff4ff374
wesley <nijie@zgyjyx.com>
2019-02-22 15:50:33 +0800

    refined code

--- thinning.py
+++ thinning.py
@@ -1,79 +1,68 @@
-"""
-===========================
-@Author  : Linbo<linbo.me>
-@Version: 1.0    25/10/2014
-This is the implementation of the 
-Zhang-Suen Thinning Algorithm for skeletonization.
-===========================
-"""
-import matplotlib
-import matplotlib.pyplot as plt
-import skimage.io as io
-"load image data"
-Img_Original =  io.imread( './data/test1.bmp')      # Gray image, rgb images need pre-conversion
+# -*- coding: utf-8 -*-
+import argparse
+import cv2
+import numpy as np
 
-"Convert gray images to binary images using Otsu's method"
-from skimage.filter import threshold_otsu
-Otsu_Threshold = threshold_otsu(Img_Original)   
-BW_Original = Img_Original < Otsu_Threshold    # must set object region as 1, background region as 0 !
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to input image")
+args = vars(ap.parse_args())
 
-def neighbours(x,y,image):
-    "Return 8-neighbours of image point P1(x,y), in a clockwise order"
-    img = image
-    x_1, y_1, x1, y1 = x-1, y-1, x+1, y+1
-    return [ img[x_1][y], img[x_1][y1], img[x][y1], img[x1][y1],     # P2,P3,P4,P5
-                img[x1][y], img[x1][y_1], img[x][y_1], img[x_1][y_1] ]    # P6,P7,P8,P9
+img = cv2.imread(args["image"], 0)
+
+retval, orig_thresh = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY_INV)
+cv2.imshow('orig_thresh', orig_thresh)
+cv2.waitKey()
+bin_thresh = (orig_thresh == 0).astype(np.uint8)
+
+
+def neighbours(x, y, image):
+    """Return 8-neighbours of point p1 of picture, in clockwise order"""
+    i = image
+    x1, y1, x_1, y_1 = x+1, y-1, x-1, y+1
+    return [i[y1][x],  i[y1][x1],   i[y][x1],  i[y_1][x1],  # P2,P3,P4,P5
+            i[y_1][x], i[y_1][x_1], i[y][x_1], i[y1][x_1]]  # P6,P7,P8,P9
+
 
 def transitions(neighbours):
-    "No. of 0,1 patterns (transitions from 0 to 1) in the ordered sequence"
-    n = neighbours + neighbours[0:1]      # P2, P3, ... , P8, P9, P2
-    return sum( (n1, n2) == (0, 1) for n1, n2 in zip(n, n[1:]) )  # (P2,P3), (P3,P4), ... , (P8,P9), (P9,P2)
+    n = neighbours + neighbours[0:1]    # P2, ... P9, P2
+    return sum((n1, n2) == (0, 1) for n1, n2 in zip(n, n[1:]))
+
 
 def zhangSuen(image):
-    "the Zhang-Suen Thinning Algorithm"
-    Image_Thinned = image.copy()  # deepcopy to protect the original image
-    changing1 = changing2 = 1        #  the points to be removed (set as 0)
-    while changing1 or changing2:   #  iterates until no further changes occur in the image
+    changing1 = changing2 = [(-1, -1)]
+    while changing1 or changing2:
         # Step 1
         changing1 = []
-        rows, columns = Image_Thinned.shape               # x for rows, y for columns
-        for x in range(1, rows - 1):                     # No. of  rows
-            for y in range(1, columns - 1):            # No. of columns
-                P2,P3,P4,P5,P6,P7,P8,P9 = n = neighbours(x, y, Image_Thinned)
-                if (Image_Thinned[x][y] == 1     and    # Condition 0: Point P1 in the object regions 
-                    2 <= sum(n) <= 6   and    # Condition 1: 2<= N(P1) <= 6
-                    transitions(n) == 1 and    # Condition 2: S(P1)=1  
-                    P2 * P4 * P6 == 0  and    # Condition 3   
-                    P4 * P6 * P8 == 0):         # Condition 4
-                    changing1.append((x,y))
-        for x, y in changing1: 
-            Image_Thinned[x][y] = 0
+        for y in range(1, len(image) - 1):
+            for x in range(1, len(image[0]) - 1):
+                P2, P3, P4, P5, P6, P7, P8, P9 = n = neighbours(x, y, image)
+                if (image[y][x] == 1 and    # (Condition 0)
+                    P4 * P6 * P8 == 0 and   # Condition 4
+                    P2 * P4 * P6 == 0 and   # Condition 3
+                    transitions(n) == 1 and  # Condition 2
+                    2 <= sum(n) <= 6):      # Condition 1
+                    changing1.append((x, y))
+        for x, y in changing1:
+            image[y][x] = 0
         # Step 2
         changing2 = []
-        for x in range(1, rows - 1):
-            for y in range(1, columns - 1):
-                P2,P3,P4,P5,P6,P7,P8,P9 = n = neighbours(x, y, Image_Thinned)
-                if (Image_Thinned[x][y] == 1   and        # Condition 0
-                    2 <= sum(n) <= 6  and       # Condition 1
-                    transitions(n) == 1 and      # Condition 2
-                    P2 * P4 * P8 == 0 and       # Condition 3
-                    P2 * P6 * P8 == 0):            # Condition 4
-                    changing2.append((x,y))    
-        for x, y in changing2: 
-            Image_Thinned[x][y] = 0
-    return Image_Thinned
- 
+        for y in range(1, len(image) - 1):
+            for x in range(1, len(image[0]) - 1):
+                P2,P3,P4,P5,P6,P7,P8,P9 = n = neighbours(x, y, image)
+                if (image[y][x] == 1 and    # (Condition 0)
+                    P2 * P6 * P8 == 0 and   # Condition 4
+                    P2 * P4 * P8 == 0 and   # Condition 3
+                    transitions(n) == 1 and  # Condition 2
+                    2 <= sum(n) <= 6):      # Condition 1
+                    changing2.append((x, y))
+        for x, y in changing2:
+            image[y][x] = 0
+    return image * 255
 
-"Apply the algorithm on images"
-BW_Skeleton = zhangSuen(BW_Original)
-# BW_Skeleton = BW_Original
-"Display the results"
-fig, ax = plt.subplots(1, 2)
-ax1, ax2 = ax.ravel()
-ax1.imshow(BW_Original, cmap=plt.cm.gray)
-ax1.set_title('Original binary image')
-ax1.axis('off')
-ax2.imshow(BW_Skeleton, cmap=plt.cm.gray)
-ax2.set_title('Skeleton of the image')
-ax2.axis('off')
-plt.show()
+
+if __name__ == '__main__':
+    after = zhangSuen(bin_thresh)
+    cv2.imshow('after', after)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
